14657e4d7c70f4659079bf50fa49673a3c850c6c
EvanaSabatella1989 <eva.sabatella@gmail.com>
2021-12-19 18:55:39 -0300

    responsive

--- script.js
+++ script.js
@@ -1,49 +1,4 @@
-// (function(){
-//     var formulario = document.getElementsByName('formulario')[0],
-//       elementos = formulario.elements,
-//       boton = document.getElementById('btn');
-//     var validarNombre = function(e) {
-//       if (formulario.nombre.value == 0){
-//         alert("Completa el campo Nombre");
-//         e.preventDefault();
-//       }
-//     }
 
-//     var validarApellido = function(e) {
-//       if (formulario.apellido.value == 0){
-//         alert("Completa el campo Apellido");
-//         e.preventDefault();
-//       }
-//     }
-
-//     var validarTelefono = function(e) {
-//       if (formulario.tel.value == 0){
-//         alert("Debe dejar un numero de Contacto");
-//         e.preventDefault();
-//       }
-//     }
-
-//     var validarMail = function(e) {
-//       if (formulario.mail.value == 0){
-//         alert("Debe dejar un mail de Contacto");
-//         e.preventDefault();
-//       }
-//     }
-
-    
-
-//     var validar = function(e) {
-//       validarNombre(e);
-//       validarApellido(e);
-//       validarTelefono(e);
-//       validarMail(e);
-      
-//     };
-
-//     formulario.addEventListener("submit",validar);
-
-   
-// }())
 
 
 
